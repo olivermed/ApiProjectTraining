ca456b131596e87647792dc9e95df992bbfe06d8
olivier <olivier.medec1@gmail.com>
2017-05-04 02:55:37 +0800

    Added token and route verifications

--- server.js
+++ server.js
@@ -15,6 +15,31 @@ var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/ProjectTrainingDataBase';
 app.listen(3000, function (){});
 
+var apiRoutes = express.Router();
+
+apiRoutes.use(function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
+});
+
+app.use('/getUsers', apiRoutes);
+
 app.set('superSecret', config.secret);
 
 //Connexion to the server
