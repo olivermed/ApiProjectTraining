ada6a2fa740bf9cd6bda3629d9cbdc51bada03f4
olivier <olivier.medec1@gmail.com>
2017-05-11 11:05:56 +0800

    Token is valid 7 days

--- server.js
+++ server.js
@@ -23,108 +23,108 @@ app.set('key', config.key);
 apiRoutes.use(function(req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
     if (token) {
-      jwt.verify(token, app.get('key'), function(err, decoded) {
-        if (err) {
-          return res.json({ success: false, message: 'Failed to authenticate token.' });
-        } else {
-          // if everything is good, save to request for use in other routes
-          req.decoded = decoded;
-          next();
-        }
-      });
+        jwt.verify(token, app.get('key'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
     } else {
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-      });
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
     }
-  });
+});
 
-  apiRoutes.get('/test', function(req, res) {
+apiRoutes.get('/test', function(req, res) {
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify("ok"));
-  });
+});
 
 app.use('/api', apiRoutes);
 
 //Connexion to the server
 MongoClient.connect(url, function (err, db) {
 
-  var User = db.collection('User');
+    var User = db.collection('User');
 
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else { //Début else
-    console.log('Connection established to', url);
-  }
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else { //Début else
+        console.log('Connection established to', url);
+    }
 
-  apiRoutes.get('/getUser/:id', function(req, res) {
-    var o_id = new mongodb.ObjectID(req.params.id);
-    dbMF.getOneCollection(req, res, User, {_id: o_id});
-  });
+    apiRoutes.get('/getUser/:id', function(req, res) {
+        var o_id = new mongodb.ObjectID(req.params.id);
+        dbMF.getOneCollection(req, res, User, {_id: o_id});
+    });
 
-  app.post('/addUser', function(req, res) {
-    dbMF.saveDocument(req, res, User);
-  });
+    app.post('/addUser', function(req, res) {
+        dbMF.saveDocument(req, res, User);
+    });
 
-  apiRoutes.get('/getUsers', function(req, res){
-    dbMF.getCollection(User, res);
-  });
+    apiRoutes.get('/getUsers', function(req, res){
+        dbMF.getCollection(User, res);
+    });
 
-  apiRoutes.get('/getUserByLogin/:login', function(req, res){
-    dbMF.getOneCollection(req, res, User, {Login: req.params.login});
-  });
+    apiRoutes.get('/getUserByLogin/:login', function(req, res){
+        dbMF.getOneCollection(req, res, User, {Login: req.params.login});
+    });
 
-  //Add friends, update user, get user by login
+    //Add friends, update user, get user by login
 
-  //Add gcm to user
-  apiRoutes.post('/addGcmId/:id/:idGcm', function(request, result) {
-    var o_id = new mongodb.ObjectID(request.params.id);
-    User.findOne({_id: o_id }, function(req, res) {
-      if (res.idGcm == undefined) {
-        User.update({_id: o_id}, {
-          $set: {idGcm: request.params.idGcm},
-          $currentDate: { lastModified: true }
+    //Add gcm to user
+    apiRoutes.post('/addGcmId/:id/:idGcm', function(request, result) {
+        var o_id = new mongodb.ObjectID(request.params.id);
+        User.findOne({_id: o_id }, function(req, res) {
+            if (res.idGcm == undefined) {
+                User.update({_id: o_id}, {
+                    $set: {idGcm: request.params.idGcm},
+                    $currentDate: { lastModified: true }
+                });
+                result.json({ message: 'ok' });
+            } else {
+                result.json({ message: 'User is already set' });
+            }
         });
-        result.json({ message: 'ok' });
-      } else {
-        result.json({ message: 'User is already set' });
-      }
     });
-  });
 
-  app.post('/login', function(req, res) {
-    console.log(req.body);
-    User.findOne({
-      Login: req.body.Login
-    }, function(err, user) {
-      if (err) throw err;
+    app.post('/login', function(req, res) {
+        console.log(req.body);
+        User.findOne({
+            Login: req.body.Login
+        }, function(err, user) {
+            if (err) throw err;
 
-      console.log("User :: ", user);
+            console.log("User :: ", user);
 
-      if (!user) {
-        res.status(400).json({ success: false, message: 'Authentication failed. User not found.' });
-      } else if (user) {
+            if (!user) {
+                res.status(400).json({ success: false, message: 'Authentication failed. User not found.' });
+            } else if (user) {
 
-        // check if password matches
-        if (user.Password != req.body.Password) {
-          res.status(400).json({ success: false, message: 'Authentication failed. Wrong password.' });
-        } else {
+                // check if password matches
+                if (user.Password != req.body.Password) {
+                    res.status(400).json({ success: false, message: 'Authentication failed. Wrong password.' });
+                } else {
 
-          // if user is found and password is right
-          // create a token
-          var token = jwt.sign(user, app.get('key'), {
-            expiresIn : 60*60*24 // expires in 24 hours
-          });
+                    // if user is found and password is right
+                    // create a token
+                    var token = jwt.sign(user, app.get('key'), {
+                        expiresIn : 60*60*24*7 // expires in 24 hours
+                    });
 
-          // return the information including token as JSON
-          res.json({
-            success: true,
-            message: 'Connected',
-            token: token
-          });
-        }
-      }
+                    // return the information including token as JSON
+                    res.json({
+                        success: true,
+                        message: 'Connected',
+                        token: token
+                    });
+                }
+            }
+        });
     });
-  });
 });
